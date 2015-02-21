be0dabe9405c496782c5c9898409624fc55157b1
Simon Blaszczynski <simone@neosb.net>
2015-02-21 11:51:40 +0100

    Fix omniatuth secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, Rails.application.secrets.meecon-facebook-id, Rails.application.secrets.meecon-facebook-secret
+  provider :facebook, Rails.application.secrets.meecon_facebook_id, Rails.application.secrets.meecon_facebook_secret
 end
 
