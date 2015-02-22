ae47d727eea755e0c1b74a1420ed313d481a70ac
Adrian <slaby.adrian@gmail.com>
2015-02-22 10:03:52 +0100

    Add validations for datetime fields in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ActiveRecord::Base
-  validates :title, :place,:beginning_at,  presence: true
-  #validates :end_at, presence: true, :greater_than => :beginning_at
+  validates :title, :place,:beginning_at, :end_at,  presence: true
+  validate :end_date_is_after_start_date
+
+  private
+
+  def end_date_is_after_start_date
+    return if beginning_at.blank? || end_at.blank?
+
+    if end_at < beginning_at
+      errors.add(:end, " date cannot be before the start date")
+    end
+  end
 end
