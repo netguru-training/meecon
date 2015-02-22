dc687d5848a76d7cde712bb34afa85f56e28b2fa
Lukasz Maslej <lukas@message.pl>
2015-02-22 14:45:55 +0100

    add respond_to block with html and js to index action in Home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @events = Event.page(params[:page])
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
