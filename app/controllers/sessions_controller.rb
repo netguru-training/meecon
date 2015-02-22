70ac3df1dc40746c8caaa27f7e0caca0d4563743
Simon Blaszczynski <simone@neosb.net>
2015-02-22 10:07:04 +0100

    Moved login logic to Authenticator form sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
-    auth_hash = request.env['omniauth.auth']
-    # render :json => auth_hash
-    @user = User.create!(
-      name: auth_hash['info']['name'], email: auth_hash['info']['email'], 
-      image: auth_hash['info']['image'], uid: auth_hash['uid'], 
-      facebook_oauth: auth_hash
-    )
-    render :json => @user
+    session['user_uid'] = Authenticator.new(auth_hash).login
+    redirect_to root_path, notice: "You have logged in successfully."
   end
+
   def failure
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
