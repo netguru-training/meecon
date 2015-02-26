041670ecb0c1b32af6b7d1240969d68d3268abde
Simon Blaszczynski <simone@neosb.net>
2015-02-26 23:06:19 +0100

    Added spec/support/database_cleaner.rb config and enabeled database_cleaner in rspec_helper. Removed comments from rspec_helper and spec_helper, and set order of specs to be random.

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,26 @@
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  # new config
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    # Seed dataase. Use it only for essential
+    # to run application data.
+    # load "#{Rails.root}/db/seeds.rb"
+  end
+
+  config.around(:each) do |example|
+    # Use really fast transaction strategy for all
+    # examples except `js: true` capybara specs
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+
+    # Start transaction
+    DatabaseCleaner.start
+
+    # Run example
+    example.run
+
+    # Rollback transaction
+    DatabaseCleaner.clean
+  end
+end
