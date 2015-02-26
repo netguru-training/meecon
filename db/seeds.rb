8a1416e748aae1e93af57672ceed50ef75157b4e
AdrianSl <slaby.adrian@gmail.com>
2015-02-26 22:32:21 +0100

    Refactor seed data, connect users with example events

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ end
 
 categories = Category.all
 
-pictures = ["http://images5.fanpop.com/image/polls/984000/984250_1332733386906_full.jpg", "https://s-media-cache-ak0.pinimg.com/736x/90/4b/84/904b84264574e016b1a01f7f7884c7e5.jpg", "https://pbs.twimg.com/profile_images/508493663538663424/48ylBWS0.jpeg", "http://www.ashlee-star.com/download/pictures/08.jpg", "http://www.ifishalaskasalmon.com/wp-content/themes/striking_r/cache/images/371_Lauren-450x250.jpg", "http://explosionhub.com/wp-content/uploads/2012/06/Stylish-Colorful-Display-Pictures-for-Girls-and-Guys-style.pk-007-450x250.jpg", "http://www.seventeen.com/cm/seventeen/images/NF/news-justin-bieber-mdn.jpg",
+pictures = ["http://images5.fanpop.com/image/polls/984000/984250_1332733386906_full.jpg", "https://s-media-cache-ak0.pinimg.com/736x/90/4b/84/904b84264574e016b1a01f7f7884c7e5.jpg", "https://pbs.twimg.com/profile_images/508493663538663424/48ylBWS0.jpeg", "http://www.ashlee-star.com/download/pictures/08.jpg", "http://www.ifishalaskasalmon.com/wp-content/themes/striking_r/cache/images/371_Lauren-450x250.jpg", "http://explosionhub.com/wp-content/uploads/2012/06/Stylish-Colorful-Display-Pictures-for-Girls-and-Guys-style.pk-007-450x250.jpg",
  "http://cdn.filmtrackonline.com/voltage/starcm_vault_root/images/%7Ba5e8dec8-73ca-4978-af89-5cae99554a05%7D/kristen_stewart_pictures_%7Bd5e824ad-e392-e111-ba52-5296b839ed91%7D.jpg"]
 
 random = Random.new
@@ -18,6 +18,7 @@ random = Random.new
     beginning_at: date,
     end_at: date + 2.days,
     category: categories.sample,
-    description: Faker::HipsterIpsum.words(250).join(' ').capitalize
+    description: Faker::HipsterIpsum.words(250).join(' ').capitalize,
+    user_id: rand(1..5)
   )
 end
