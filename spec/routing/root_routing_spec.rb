d58654c26ea19ac20c5d4ec379d16ed25ad1d8df
Simon Blaszczynski <simone@neosb.net>
2015-02-27 02:03:19 +0100

    Removed unnecessary comment.

--- spec/routing/root_routing_spec.rb
+++ spec/routing/root_routing_spec.rb
@@ -1,13 +1,5 @@
 require 'rails_helper'
 
-# describe HomeController do
-#   describe 'GET root_path' do
-#     before { get root_path }
-#
-#     it { should respond_with(200) }
-#   end
-# end
-
 describe "routes for root of app" do
   it "routes / to the home controller index action" do
     expect(get: "/").to route_to(controller: "home", action: "index")
