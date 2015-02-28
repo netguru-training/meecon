657f75e4253c9786343a794f5edea5f3cfd04411
Simon Blaszczynski <simone@neosb.net>
2015-02-28 14:16:28 +0100

    Removed unnecessary comments.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,23 +17,18 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    # Seed dataase. Use it only for essential
-    # to run application data.
+    
+    # uncomment next line to seed databse before tests
     # load "#{Rails.root}/db/seeds.rb"
   end
 
   config.around(:each) do |example|
-    # Use really fast transaction strategy for all
-    # examples except `js: true` capybara specs
     DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
 
-    # Start transaction
     DatabaseCleaner.start
 
-    # Run example
     example.run
 
-    # Rollback transaction
     DatabaseCleaner.clean
   end
 end
