feb7480dd6f3f9da1b6467bf6800f3111bec97a3
Simon Blaszczynski <simone@neosb.net>
2015-02-28 12:56:01 +0100

    Removed spec/support/* and moved code from database_cleaner to rails_helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
-require 'support/database_cleaner'
+require 'database_cleaner'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -14,4 +14,26 @@ RSpec.configure do |config|
 
 
   config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    # Seed dataase. Use it only for essential
+    # to run application data.
+    # load "#{Rails.root}/db/seeds.rb"
+  end
+
+  config.around(:each) do |example|
+    # Use really fast transaction strategy for all
+    # examples except `js: true` capybara specs
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+
+    # Start transaction
+    DatabaseCleaner.start
+
+    # Run example
+    example.run
+
+    # Rollback transaction
+    DatabaseCleaner.clean
+  end
 end
