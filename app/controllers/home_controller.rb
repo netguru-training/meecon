b5eaec7f00a2b34b04291064f7f044e622b1ce8f
AdrianSl <slaby.adrian@gmail.com>
2015-02-28 01:42:59 +0100

    Small fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.coming.not_ended.page(params[:page])
+    @events = Event.coming.not_ended.page(params[:page]).decorate
     respond_to do |format|
       format.html
       format.js
--- app/decorators/paginating_decorator.rb
+++ app/decorators/paginating_decorator.rb
@@ -1,3 +1,3 @@
 class PaginatingDecorator < Draper::CollectionDecorator
-  delegate :current_page, :total_pages, :limit_value, :total_count
+  delegate :current_page, :total_pages, :limit_value
 end
