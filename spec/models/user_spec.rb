76cec1910bc9860b6075a3c9126a5904b37cafec
Simon Blaszczynski <simone@neosb.net>
2015-03-01 02:46:54 +0100

    user_spec added for testing User model validations. Changed double quotes to apostrophes in root_routing_spec.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe User do
+  it 'validates presence of fields' do
+    expect(User.new).to validate_presence_of :uid
+    expect(User.new).to validate_presence_of :image
+    expect(User.new).to validate_presence_of :name
+    expect(User.new).to validate_presence_of :email
+    expect(User.new).to validate_presence_of :facebook_oauth
+  end
+end
--- spec/routing/root_routing_spec.rb
+++ spec/routing/root_routing_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "routes for root of app" do
-  it "routes / to the home controller index action" do
-    expect(get: "/").to route_to(controller: "home", action: "index")
+describe 'routes for root of app' do
+  it 'routes / to the home controller index action' do
+    expect(get: '/').to route_to(controller: 'home', action: 'index')
   end
 end
