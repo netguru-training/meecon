e3f36c7be6e4d3ff4ff7a60ad834f5d92883dde0
Simon Blaszczynski <simone@neosb.net>
2015-03-01 02:29:50 +0100

    Add user model validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
+# Model for user logged in with Faebook
 class User < ActiveRecord::Base
   has_many :events
   serialize :facebook_oauth
+  validates :uid, presence: true
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :facebook_oauth, presence: true
 end
