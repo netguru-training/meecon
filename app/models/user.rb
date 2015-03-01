475a45f1ffd1b474be1251b8da3479dac83fde6e
AdrianSl <slaby.adrian@gmail.com>
2015-03-01 12:18:46 +0100

    Add validation to users uid not to be null in users model and database, add user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :events
   serialize :facebook_oauth
+
+  validates :uid, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222141423) do
+ActiveRecord::Schema.define(version: 20150301110801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20150222141423) do
     t.string   "email"
     t.string   "name"
     t.string   "image"
-    t.string   "uid"
+    t.string   "uid",            null: false
     t.text     "facebook_oauth"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
